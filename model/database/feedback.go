c17372b17d194041d32b377b5212a1881dda3267
Billdex <pc.luoxin@foxmail.com>
2020-11-30 19:47:02 +0800

    反馈功能记录QQ号的数据库字段类型修改为BIGINT

--- model/database/feedback.go
+++ model/database/feedback.go
@@ -3,7 +3,7 @@ package database
 import "time"
 
 type Feedback struct {
-	Sender     int       `xorm:"sender"`
+	Sender     int       `xorm:"sender BIGINT"`
 	Nickname   string    `xorm:"nickname"`
 	Message    string    `xorm:"message"`
 	CreateTime time.Time `xorm:"create_time created"`
