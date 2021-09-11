349d4a13b3979a6940b1c76677099b2814f6afa8
billdex <pc.luoxin@foxmail.com>
2021-09-12 02:25:07 +0800

    实现私聊功能的启用与关闭

---
+++ messageservice/private_state.go
@@ -0,0 +1,58 @@
+package messageservice
+
+import (
+	"bcjh-bot/global"
+	"bcjh-bot/scheduler"
+	"bcjh-bot/scheduler/onebot"
+	"bcjh-bot/util/logger"
+)
+
+func AllowPrivate(c *scheduler.Context) {
+	if c.GetMessageType() == onebot.MessageTypePrivate {
+		event := c.GetGroupEvent()
+		if err := global.SetBotState(event.SelfId, 0, true); err != nil {
+			logger.Error("启用机器人私聊出错:", err)
+			return
+		}
+		_, _ = c.Reply("已允许使用私聊")
+	} else if c.GetMessageType() == onebot.MessageTypeGroup {
+		atList := c.GetAtList()
+		for _, at := range atList {
+			if at == c.GetBot().BotId {
+				event := c.GetGroupEvent()
+				if err := global.SetBotState(event.SelfId, 0, true); err != nil {
+					logger.Error("启用机器人私聊出错:", err)
+					return
+				}
+				_, _ = c.Reply("已允许使用私聊")
+				break
+			}
+		}
+	}
+}
+
+func DisablePrivate(c *scheduler.Context) {
+	if c.GetMessageType() == onebot.MessageTypePrivate {
+		event := c.GetGroupEvent()
+		if err := global.SetBotState(event.SelfId, 0, false); err != nil {
+			logger.Error("禁用机器人私聊出错:", err)
+			_, _ = c.Reply("禁用失败")
+			return
+		}
+		_, _ = c.Reply("已禁用私聊查询")
+	} else if c.GetMessageType() == onebot.MessageTypeGroup {
+		atList := c.GetAtList()
+		for _, at := range atList {
+			if at == c.GetBot().BotId {
+				event := c.GetGroupEvent()
+				if err := global.SetBotState(event.SelfId, 0, false); err != nil {
+					logger.Error("禁用机器人私聊出错:", err)
+					_, _ = c.Reply("禁用失败")
+					return
+				}
+				_, _ = c.Reply("已禁用私聊查询")
+				break
+			}
+		}
+	}
+}
