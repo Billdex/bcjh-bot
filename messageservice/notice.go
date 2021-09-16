6af7438d331061a393a1498d65746133a09749e2
billdex <pc.luoxin@foxmail.com>
2021-09-16 19:11:00 +0800

    修复公告功能获取群列表的问题

--- messageservice/notice.go
+++ messageservice/notice.go
@@ -10,7 +10,7 @@ func PublicNotice(c *scheduler.Context) {
 	msg := c.PretreatedMessage
 	bots := c.GetLinkBotList()
 	for _, bot := range bots {
-		groups, err := c.GetBot().GetGroupList()
+		groups, err := bot.GetGroupList()
 		if err != nil {
 			_, _ = c.Reply(fmt.Sprintf("未获取[bot %d]到group列表, err:%v", bot.BotId, err))
 		}
