ece8ebb87a748c8c1b50630eb4f5386b8ec9f78c
billdex <pc.luoxin@foxmail.com>
2022-06-11 10:15:54 +0800

    更新白菜菊花网址与二维码，小鱼做好DNS解析了不用再分两个了

--- messageservice/shortcut.go
+++ messageservice/shortcut.go
@@ -11,12 +11,12 @@ import (
 func GalleryWebsite(c *scheduler.Context) {
 	var msg string
 	foodgame := "https://foodgame.gitee.io/"
-	bcjh := "https://bcjh.pages.dev/"
-	bcjhxyz := "https://www.bcjh.xyz/"
+	//bcjh := "https://bcjh.pages.dev/"
+	bcjh := "https://www.bcjh.xyz/"
 
 	msg += fmt.Sprintf("L图鉴网: %s\n", foodgame)
-	msg += fmt.Sprintf("白菜菊花手机图鉴网: %s\n", bcjh)
-	msg += fmt.Sprintf("白菜菊花备用网址: %s", bcjhxyz)
+	//msg += fmt.Sprintf("白菜菊花手机图鉴网: %s\n", bcjh)
+	msg += fmt.Sprintf("白菜菊花: %s", bcjh)
 
 	_, _ = c.Reply(msg)
 }
@@ -30,26 +30,26 @@ func TermInfo(c *scheduler.Context) {
 
 // 白菜菊花App下载
 func BCJHAppDownload(c *scheduler.Context) {
-	imgPathPage := config.AppConfig.Resource.Shortcut + "/白菜菊花page.jpg"
-	imgPathXyz := config.AppConfig.Resource.Shortcut + "/白菜菊花xyz.jpg"
+	imgPath := config.AppConfig.Resource.Shortcut + "/白菜菊花.jpg"
+	//imgPathXyz := config.AppConfig.Resource.Shortcut + "/白菜菊花xyz.jpg"
 	var msg string
-	msg += onebot.GetCQImage(imgPathPage, "file")
-	msg += onebot.GetCQImage(imgPathXyz, "file")
+	msg += onebot.GetCQImage(imgPath, "file")
+	//msg += onebot.GetCQImage(imgPathXyz, "file")
 
 	_, _ = c.Reply(msg)
 }
 
 // 计算器，不用第二遍解释
 func Calculator(c *scheduler.Context) {
-	bcjh := "https://bcjh.pages.dev/"
-	bcjhxyz := "https://www.bcjh.xyz/"
-	imgPathPage := config.AppConfig.Resource.Shortcut + "/白菜菊花page.jpg"
-	imgPathXyz := config.AppConfig.Resource.Shortcut + "/白菜菊花xyz.jpg"
+	//bcjh := "https://bcjh.pages.dev/"
+	bcjh := "https://www.bcjh.xyz/"
+	//imgPathPage := config.AppConfig.Resource.Shortcut + "/白菜菊花page.jpg"
+	//imgPathXyz := config.AppConfig.Resource.Shortcut + "/白菜菊花xyz.jpg"
+	imgPath := config.AppConfig.Resource.Shortcut + "/白菜菊花.jpg"
 	var msg string
 	msg += fmt.Sprintf("网页版计算器在白菜菊花图鉴网:%s\n", bcjh)
-	msg += fmt.Sprintf("白菜菊花备用网址: %s\n", bcjhxyz)
-	msg += fmt.Sprintf("安卓用户支持使用白菜菊花app，扫描下图二维码下载 %s %s",
-		onebot.GetCQImage(imgPathPage, "file"),
-		onebot.GetCQImage(imgPathXyz, "file"))
+	msg += fmt.Sprintf("安卓用户支持使用白菜菊花app，扫描下图二维码下载 %s",
+		onebot.GetCQImage(imgPath, "file"))
+	//onebot.GetCQImage(imgPathXyz, "file"))
 	_, _ = c.Reply(msg)
 }
