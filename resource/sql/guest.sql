cbbffd59515d70de69f3c6e5bd17abd67f635358
billdex <pc.luoxin@foxmail.com>
2022-08-01 15:04:43 +0800

    修改预导入 sql 数据形式

--- resource/sql/guest.sql
+++ resource/sql/guest.sql
@@ -1,62 +1,56 @@
-create table guest
-(
-    guest_id   varchar(255) null,
-    guest_name varchar(255) null
-)
-    comment '贵客id名称对照表';
-
-INSERT INTO guest (guest_id, guest_name) VALUES ('001', '耶稣');
-INSERT INTO guest (guest_id, guest_name) VALUES ('002', '如来');
-INSERT INTO guest (guest_id, guest_name) VALUES ('003', '木良');
-INSERT INTO guest (guest_id, guest_name) VALUES ('004', '木优');
-INSERT INTO guest (guest_id, guest_name) VALUES ('005', '迷思吹法师');
-INSERT INTO guest (guest_id, guest_name) VALUES ('006', '大胃王');
-INSERT INTO guest (guest_id, guest_name) VALUES ('007', '屠夫老王');
-INSERT INTO guest (guest_id, guest_name) VALUES ('008', '丐帮帮主');
-INSERT INTO guest (guest_id, guest_name) VALUES ('009', '潇洒李白');
-INSERT INTO guest (guest_id, guest_name) VALUES ('010', '迷弟杜甫');
-INSERT INTO guest (guest_id, guest_name) VALUES ('011', '诸葛亮');
-INSERT INTO guest (guest_id, guest_name) VALUES ('012', '黄月英');
-INSERT INTO guest (guest_id, guest_name) VALUES ('013', '苏轼');
-INSERT INTO guest (guest_id, guest_name) VALUES ('014', '唐伯虎');
-INSERT INTO guest (guest_id, guest_name) VALUES ('015', '张飞');
-INSERT INTO guest (guest_id, guest_name) VALUES ('016', '关羽');
-INSERT INTO guest (guest_id, guest_name) VALUES ('017', '刘备');
-INSERT INTO guest (guest_id, guest_name) VALUES ('018', '吕布');
-INSERT INTO guest (guest_id, guest_name) VALUES ('019', '貂蝉');
-INSERT INTO guest (guest_id, guest_name) VALUES ('020', '大内侍卫');
-INSERT INTO guest (guest_id, guest_name) VALUES ('021', '王子');
-INSERT INTO guest (guest_id, guest_name) VALUES ('022', '公主');
-INSERT INTO guest (guest_id, guest_name) VALUES ('023', '大明湖畔的美女');
-INSERT INTO guest (guest_id, guest_name) VALUES ('024', '皇帝');
-INSERT INTO guest (guest_id, guest_name) VALUES ('025', '皇后');
-INSERT INTO guest (guest_id, guest_name) VALUES ('026', '官爷');
-INSERT INTO guest (guest_id, guest_name) VALUES ('027', '富人');
-INSERT INTO guest (guest_id, guest_name) VALUES ('028', '富太太');
-INSERT INTO guest (guest_id, guest_name) VALUES ('029', '富二代');
-INSERT INTO guest (guest_id, guest_name) VALUES ('030', '白富美');
-INSERT INTO guest (guest_id, guest_name) VALUES ('031', 'TapTap');
-INSERT INTO guest (guest_id, guest_name) VALUES ('032', '圣诞老人');
-INSERT INTO guest (guest_id, guest_name) VALUES ('033', '屈原');
-INSERT INTO guest (guest_id, guest_name) VALUES ('034', '王母娘娘');
-INSERT INTO guest (guest_id, guest_name) VALUES ('035', '武松');
-INSERT INTO guest (guest_id, guest_name) VALUES ('036', '伯爵大人');
-INSERT INTO guest (guest_id, guest_name) VALUES ('037', '西瓜小哥');
-INSERT INTO guest (guest_id, guest_name) VALUES ('038', '福神');
-INSERT INTO guest (guest_id, guest_name) VALUES ('039', '禄神');
-INSERT INTO guest (guest_id, guest_name) VALUES ('040', '寿神');
-INSERT INTO guest (guest_id, guest_name) VALUES ('041', '罗爸爸');
-INSERT INTO guest (guest_id, guest_name) VALUES ('042', '罗妈妈');
-INSERT INTO guest (guest_id, guest_name) VALUES ('043', '黑道小三');
-INSERT INTO guest (guest_id, guest_name) VALUES ('044', '黑道小伊');
-INSERT INTO guest (guest_id, guest_name) VALUES ('045', '小宝');
-INSERT INTO guest (guest_id, guest_name) VALUES ('046', '容嬷嬷');
-INSERT INTO guest (guest_id, guest_name) VALUES ('047', '太上皇');
-INSERT INTO guest (guest_id, guest_name) VALUES ('048', '耶稣如来');
-INSERT INTO guest (guest_id, guest_name) VALUES ('049', '木良木优');
-INSERT INTO guest (guest_id, guest_name) VALUES ('050', '潇洒李白迷弟杜甫');
-INSERT INTO guest (guest_id, guest_name) VALUES ('051', '诸葛亮黄月英');
-INSERT INTO guest (guest_id, guest_name) VALUES ('052', '张飞关羽刘备');
-INSERT INTO guest (guest_id, guest_name) VALUES ('053', '吕布貂蝉');
-INSERT INTO guest (guest_id, guest_name) VALUES ('054', '福禄寿三神');
-INSERT INTO guest (guest_id, guest_name) VALUES ('055', '不良青年');
+INSERT INTO `guest` (guest_id, guest_name)
+VALUES ('001', '耶稣'),
+       ('002', '如来'),
+       ('003', '木良'),
+       ('004', '木优'),
+       ('005', '迷思吹法师'),
+       ('006', '大胃王'),
+       ('007', '屠夫老王'),
+       ('008', '丐帮帮主'),
+       ('009', '潇洒李白'),
+       ('010', '迷弟杜甫'),
+       ('011', '诸葛亮'),
+       ('012', '黄月英'),
+       ('013', '苏轼'),
+       ('014', '唐伯虎'),
+       ('015', '张飞'),
+       ('016', '关羽'),
+       ('017', '刘备'),
+       ('018', '吕布'),
+       ('019', '貂蝉'),
+       ('020', '大内侍卫'),
+       ('021', '王子'),
+       ('022', '公主'),
+       ('023', '大明湖畔的美女'),
+       ('024', '皇帝'),
+       ('025', '皇后'),
+       ('026', '官爷'),
+       ('027', '富人'),
+       ('028', '富太太'),
+       ('029', '富二代'),
+       ('030', '白富美'),
+       ('031', 'TapTap'),
+       ('032', '圣诞老人'),
+       ('033', '屈原'),
+       ('034', '王母娘娘'),
+       ('035', '武松'),
+       ('036', '伯爵大人'),
+       ('037', '西瓜小哥'),
+       ('038', '福神'),
+       ('039', '禄神'),
+       ('040', '寿神'),
+       ('041', '罗爸爸'),
+       ('042', '罗妈妈'),
+       ('043', '黑道小三'),
+       ('044', '黑道小伊'),
+       ('045', '小宝'),
+       ('046', '容嬷嬷'),
+       ('047', '太上皇'),
+       ('048', '耶稣如来'),
+       ('049', '木良木优'),
+       ('050', '潇洒李白迷弟杜甫'),
+       ('051', '诸葛亮黄月英'),
+       ('052', '张飞关羽刘备'),
+       ('053', '吕布貂蝉'),
+       ('054', '福禄寿三神'),
+       ('055', '不良青年');
