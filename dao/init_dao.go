013ed3fa35ec2942b4d0f13f8aa766cbc6190775
billdex <pc.luoxin@foxmail.com>
2022-08-08 00:39:41 +0800

    优化配置文件导入流程，go版本升级到1.17

--- dao/init_dao.go
+++ dao/init_dao.go
@@ -16,11 +16,17 @@ var DB *xorm.Engine
 // [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
 
 // InitDatabase 初始化数据库连接
-func InitDatabase(dsn string) error {
+func InitDatabase() error {
 	var err error
 	if config.AppConfig.DB.UseLocal {
 		DB, err = xorm.NewEngine("sqlite", "./bot_data.db")
 	} else {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&loc=Local",
+			config.AppConfig.DB.User,
+			config.AppConfig.DB.Password,
+			config.AppConfig.DB.Host,
+			config.AppConfig.DB.Database,
+		)
 		DB, err = xorm.NewEngine("mysql", dsn)
 	}
 	if err != nil {
