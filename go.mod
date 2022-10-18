4dcbfd0509aac7441e10253930efa19457f37e24
Billdex <pc.luoxin@foxmail.com>
2022-10-18 18:01:35 +0800

    优化符文查询，go 版本升级1.19，使用泛型重写分页输出

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module bcjh-bot
 
-go 1.17
+go 1.19
 
 require (
 	github.com/allegro/bigcache/v3 v3.0.2
--- messageservice/antique.go
+++ messageservice/antique.go
@@ -10,6 +10,8 @@ import (
 	"bcjh-bot/util/e"
 	"bcjh-bot/util/logger"
 	"fmt"
+	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -18,30 +20,43 @@ func AntiqueQuery(c *scheduler.Context) {
 	args := strings.Split(c.PretreatedMessage, " ")
 	antique := args[0]
 
-	guests := make([]database.GuestGift, 0)
-	err := dao.DB.Where("antique like ?", "%"+antique+"%").Asc("total_time").Find(&guests)
+	re, err := regexp.Compile(strings.ReplaceAll(antique, "%", ".*"))
 	if err != nil {
-		logger.Error("查询数据库出错!", err)
-		_, _ = c.Reply(e.SystemErrorNote)
-		return
-	}
-	if len(guests) == 0 {
-		_, _ = c.Reply("没有找到符文数据")
+		_, _ = c.Reply("查询格式有误")
 		return
 	}
 
-	antiqueMap := make(map[string]string)
-	for _, guest := range guests {
-		antiqueMap[guest.Antique] = guest.Antique
+	guestGifts, err := dao.FindAllGuestGifts()
+	if err != nil {
+		logger.Error("查询贵客礼物数据出错!")
+		_, _ = c.Reply(e.SystemErrorNote)
+		return
+	}
+
+	antiqueMap := make(map[string]struct{})
+	gifts := make([]database.GuestGift, 0)
+	for _, guestGift := range guestGifts {
+		if re.MatchString(guestGift.Antique) {
+			antiqueMap[guestGift.Antique] = struct{}{}
+			gifts = append(gifts, guestGift)
+		}
+	}
+	if len(antiqueMap) == 0 {
+		_, _ = c.Reply("没有找到符文数据")
+		return
 	}
 	if len(antiqueMap) > 1 {
-		msg := "你要找哪个符文的数据呢:"
-		for _, v := range antiqueMap {
-			msg += fmt.Sprintf("\n%s", v)
+		msg := "你要找哪个符文的数据呢"
+		for antique := range antiqueMap {
+			msg += fmt.Sprintf("\n%s", antique)
 		}
 		_, _ = c.Reply(msg)
 		return
 	}
+	// 最终结果按照从菜谱总耗时升序排列
+	sort.Slice(gifts, func(i, j int) bool {
+		return gifts[i].TotalTime < gifts[j].TotalTime
+	})
 
 	page := 1
 	if len(args) > 1 {
@@ -58,27 +73,15 @@ func AntiqueQuery(c *scheduler.Context) {
 		}
 	}
 
-	var msg string
 	listLength := config.AppConfig.Bot.GroupMsgMaxLen
 	if c.GetMessageType() == onebot.MessageTypePrivate {
 		listLength = config.AppConfig.Bot.PrivateMsgMaxLen
 	}
-	maxPage := (len(guests)-1)/listLength + 1
-	if len(guests) > listLength {
-		if page > maxPage {
-			page = maxPage
-		}
-		msg += fmt.Sprintf("以下菜有概率得%s: (%d/%d)", guests[0].Antique, page, maxPage)
-	} else {
-		msg += fmt.Sprintf("以下菜有概率得%s:", guests[0].Antique)
-	}
-	for i := (page - 1) * listLength; i < page*listLength && i < len(guests); i++ {
-		totalTime := util.FormatSecondToString(guests[i].TotalTime)
-		msg += fmt.Sprintf("\n%s-%s %s", guests[i].Recipe, guests[i].GuestName, totalTime)
-	}
-	if page < maxPage {
-		msg += "\n......"
-	}
+	msg := util.PaginationOutput(gifts, page, listLength,
+		fmt.Sprintf("以下菜谱概率获得%s", gifts[0].Antique),
+		func(gift database.GuestGift) string {
+			return fmt.Sprintf("%s-%s %s", gift.Recipe, gift.GuestName, util.FormatSecondToString(gift.TotalTime))
+		})
 
 	_, _ = c.Reply(msg)
 }
